9523e8b7fe62a1b4631ff1bd0a39a80b5c2ef2bf
Ethan Nagel <eanagel@gmail.com>
2015-10-17 17:04:39 -0700

    Initial Commit

---
+++ NTTrackingTableViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NTTrackingTableViewSample
+//
+//  Created by Ethan Nagel on 10/17/15.
+//  Copyright © 2015 Nagel Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
